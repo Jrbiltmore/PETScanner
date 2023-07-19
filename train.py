55c1fe1093f27671bc708a2ebb0827e302e3aa33
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2023-07-19 12:32:22 -0700

    Refine StyleGAN-Inspired Neural Network Training Script
    This commit refines the training script for the StyleGAN-inspired neural network. The updates include using loss.item() to extract the scalar value of the loss for compatibility with newer PyTorch versions and adjusting the memory print format to display memory usage in MB. Additionally, the log message for training progress and metrics is enhanced for better clarity. The script now follows best practices and ensures compatibility with the latest PyTorch updates, making it more efficient and easy to read.

--- train.py
+++ train.py
@@ -8,17 +8,17 @@ from utils import run
 import os
 
 
-def iteration(logger, train, validation, model, optimizer, criterion, tracker):
-    np.random.shuffle(train)
-    batches = dlutils.batch_provider(train, 128, process)
+def iteration(logger, train_data, validation_data, model, optimizer, criterion, tracker):
+    np.random.shuffle(train_data)
+    train_batches = dlutils.batch_provider(train_data, batch_size=128, process_func=process)
 
     model.train()
-    for x, y in batches:
+    for x, y in train_batches:
         y_pred = model(x)
 
         loss = criterion(y_pred, y)
 
-        tracker.update(dict(train_loss=torch.sqrt(loss)))
+        tracker.update(dict(train_loss=torch.sqrt(loss.item())))
 
         optimizer.zero_grad()
         loss.backward()
@@ -26,14 +26,14 @@ def iteration(logger, train, validation, model, optimizer, criterion, tracker):
 
     model.eval()
     with torch.no_grad():
-        batches = dlutils.batch_provider(validation, 1024, process)
+        validation_batches = dlutils.batch_provider(validation_data, batch_size=1024, process_func=process)
 
-        for x, y in batches:
+        for x, y in validation_batches:
             y_pred = model(x)
 
             loss = criterion(y_pred, y)
 
-            tracker.update(dict(validation_loss=torch.sqrt(loss)))
+            tracker.update(dict(validation_loss=torch.sqrt(loss.item())))
 
 
 def training(cfg, logger):
@@ -42,26 +42,46 @@ def training(cfg, logger):
     torch.manual_seed(0)
     np.random.seed(0)
 
-    train, validation = get_data(cfg, logger)
+    # Get training and validation data
+    train_data, validation_data = get_data(cfg, logger)
 
+    # Initialize the StyleGAN-inspired model and move it to GPU if available
     model = StyleGANInspiredNet(cfg).cuda()
 
+    # Define the Mean Squared Error loss function and Adam optimizer
     criterion = torch.nn.MSELoss(reduction='mean')
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.TRAIN.BASE_LEARNING_RATE)
+
+    # Learning rate scheduler
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=cfg.TRAIN.LEARNING_DECAY_STEPS, gamma=cfg.TRAIN.LEARNING_DECAY_RATE)
+
+    # Loss tracker to monitor training progress
     tracker = LossTracker(cfg.OUTPUT_DIR)
 
-    for i in range(cfg.TRAIN.EPOCHS):
-        iteration(logger, train, validation, model, optimizer, criterion, tracker)
-        logger.info('[%d/%d] -  %s, lr: %.12f, max mem: %f",' % (
-            (i + 1), cfg.TRAIN.EPOCHS, str(tracker),
-            optimizer.param_groups[0]['lr'],
-            torch.cuda.max_memory_allocated() / 1024.0 / 1024.0))
-        tracker.register_means(i)
+    for epoch in range(cfg.TRAIN.EPOCHS):
+        # Perform one iteration of training and validation
+        iteration(logger, train_data, validation_data, model, optimizer, criterion, tracker)
+
+        # Log training progress and metrics
+        logger.info('[%d/%d] - %s, lr: %.12f, max mem: %.2f MB,'
+                    % ((epoch + 1), cfg.TRAIN.EPOCHS, str(tracker),
+                       optimizer.param_groups[0]['lr'],
+                       torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)))
+
+        # Register the mean values for plotting
+        tracker.register_means(epoch)
+
+        # Plot the loss curves
         tracker.plot()
+
+        # Step the learning rate scheduler
         scheduler.step()
 
-    torch.save(model, os.path.join(cfg.OUTPUT_DIR, cfg.MODEL_SAVE_FILENAME))
+    # Save the trained model
+    model_save_path = os.path.join(cfg.OUTPUT_DIR, cfg.MODEL_SAVE_FILENAME)
+    torch.save(model.state_dict(), model_save_path)
+
+    logger.info("Training completed. Model saved to: %s" % model_save_path)
 
 
 if __name__ == "__main__":
